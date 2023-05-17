4fd869a5009cc33a2fc21024b67f3d31ee3f3595
= <=>
2023-05-17 08:26:45 -0500

    Version 1

---
+++ inter.js
@@ -0,0 +1,13 @@
+const img = document.getElementById("reg_click");
+const img_2 = document.getElementById("propuesta");
+
+
+img.addEventListener("click", ()=>{
+
+    alert('Alichis....');
+if(img_2.style.display="none"){
+    img_2.setAttribute("style","display:inline;");
+}
+
+
+});
